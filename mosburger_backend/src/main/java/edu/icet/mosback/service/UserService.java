fa1d9f48ca6bf13f873e3aa943c92d320782371e
Minindu Alwis <minindualwis@gmail.com>
2025-03-08 19:07:14 +0530

    burger pos

--- mosburger_backend/src/main/java/edu/icet/mosback/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.mosback.service;
-
-import edu.icet.mosback.entity.User;
-
-public interface UserService {
-    User registerUser(User user);
-
-    boolean authenticateUser(String username, String password);
-}
